d2866194bdcfcb38a7e40dd53e2bdbeaf526a7ae
andreselcientifico <andresperezrodriguez28@gmail.com>
2024-05-13 17:19:47 -0500

    api_url

--- rxconfig.py
+++ rxconfig.py
@@ -6,5 +6,6 @@ load_dotenv('.env')
 
 config = rx.Config(
     app_name="pdf_translate_web_reflex",
+    api_url="http://localhost",
     # db_url=f"postgresql+psycopg://pyrumind:{os.getenv('PASSWORD_DB')}@{os.getenv('IP_DB')}:5432/pyrumind",
 )
