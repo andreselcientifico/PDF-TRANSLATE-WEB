176e5d1029079ea60a339ebf427163a0285ce9be
andreselcientifico <andresperezrodriguez28@gmail.com>
2024-05-13 17:43:36 -0500

    Docker

--- rxconfig.py
+++ rxconfig.py
@@ -6,6 +6,6 @@ load_dotenv('.env')
 
 config = rx.Config(
     app_name="pdf_translate_web_reflex",
-    api_url="http://localhost",
+    api_url="http://localhost:8000",
     # db_url=f"postgresql+psycopg://pyrumind:{os.getenv('PASSWORD_DB')}@{os.getenv('IP_DB')}:5432/pyrumind",
 )
